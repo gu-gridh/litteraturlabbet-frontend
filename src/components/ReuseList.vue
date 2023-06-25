7bebc4617ee269a19d1c9825721141aec4d77ec7
Jonathan Westin <70022179+jonathanwestin@users.noreply.github.com>
2023-06-25 22:47:27 +0200

    Update ReuseList.vue

--- src/components/ReuseList.vue
+++ src/components/ReuseList.vue
@@ -200,7 +200,7 @@ watch(
 
 .author-name,
 .work-title {
-  background-color: rgb(180,100,100);
+  background-color: darkorange;
   color: white;
   padding: 0.2rem 0.5rem 0.3rem 0.5rem;
   border-radius: 8px;
