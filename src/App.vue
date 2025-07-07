1cdab5856348f366a242f5cbbcc1250d3993f8a6
David Alfter <david.alfter@gu.se>
2025-07-07 17:38:03 +0200

    Added manual page.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="super-container">
     <Waiter></Waiter>
+    <Manual v-if="manualVisible" @close-manual="toggleManual"></Manual>
     <div class="top">
       <a href="https://litteraturbanken.se">
         <div class="lb-logo-container"></div>
@@ -17,8 +18,14 @@
         <div class="nav-item sub-title"><router-link to="/reuse/" class="nav-link">• TEXTÅTERBRUK</router-link>
           <div class="nav-ornament" v-bind:class="{active: isActive('/reuse/')}"></div>
         </div>
+        
       </div>
     </div>
+    <div class="manual-toggle-container">
+          <div class="manual-link" @click="toggleManual">
+            ?
+          </div>
+        </div>
     <div class="main-container">
       <div class="left-column">
         <Suspense>
@@ -48,6 +55,16 @@ import { RouterLink, RouterView, useRoute } from "vue-router";
 import Footer from "@/components/Footer.vue";
 import CollectionSearch from "./components/CollectionSearch.vue";
 import Waiter from "@/components/Waiter.vue";
+import Manual from "@/components/Manual.vue";
+import { ref } from "vue";
+const manualVisible = ref(false);
+const toggleManual = () => {
+  manualVisible.value = !manualVisible.value;
+  // Scroll to top when manual is opened
+  if (manualVisible.value) {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+};
 const route = useRoute();
 const isActive = (routeName: string) => {
   if (route.path === "/") {
@@ -62,11 +79,39 @@ const isActive = (routeName: string) => {
   opacity: 1;
 }
 
+
 .nav-content .title {
   padding-top: 0px;
 }
 
 .nav-content .sub-title {
   padding-left: 0px;
+}
+
+.manual-toggle-container {
+  position: absolute;
+  top: 80px;
+  right: 50px;
+  z-index: 50;
+}
+
+.manual-link {
+  width: 60px;
+  height: 60px;
+  background-color: var(--theme-main-color);
+  color: var(--color-background-soft);
+  border-radius: 50%;
+  text-align: center;
+  line-height: 60px;
+  font-weight: bold;
+  font-size: 24px;
+  cursor: pointer;
+  font-family: "Cormorant Garamond", serif;
+  
+  transition: background-color 0.3s ease;
+}
+
+.manual-link:hover {
+  background-color: var(--theme-main-color-light);
 }
 </style>
